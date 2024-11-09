e115d0cdd309fdc6a36cfdbfeb3bfdb7ec5b27b7
Tali Auster <me@tali.network>
2024-11-09 11:16:52 -0800

    feat: add flake outputs lib.buildSite and test-site
    So begins integration with Gardiner.

--- flake.nix
+++ flake.nix
@@ -3,17 +3,47 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
 
   outputs = { self, alirenix, nixpkgs }: {
-    packages.x86_64-linux.default =
+    packages.x86_64-linux =
       let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in (alirenix.lib.buildAlireCrate {
-        inherit pkgs;
-      }) {
-        src = ./.;
-        pname = "passel";
-        version = "0.1.0";
-        index = alirenix.packages.x86_64-linux.community-index;
-        alire = alirenix.packages.x86_64-linux.alire;
-        depsHash = "sha256-OW/RDzX+76poy6K9LJqcSoTHaDjAbvuBw6K0KCR0BtQ=";
+      in {
+        default = alirenix.lib.buildAlireCrate ({
+          inherit pkgs;
+        }) {
+          src = ./.;
+          pname = "passel";
+          version = "0.1.0";
+          index = alirenix.packages.x86_64-linux.community-index;
+          alire = alirenix.packages.x86_64-linux.alire;
+          depsHash = "sha256-OW/RDzX+76poy6K9LJqcSoTHaDjAbvuBw6K0KCR0BtQ=";
+        };
+
+        test-site = pkgs.callPackage self.lib.buildSite {
+          src = ./res;
+          passel = self.packages.x86_64-linux.default;
+          version = "0.1.0";
+        };
       };
+
+    lib = {
+      buildSite =
+        { src
+        , passel
+        , pname ? "passel-site"
+        , version
+        , stdenv
+        }: stdenv.mkDerivation {
+          inherit pname version src;
+
+          buildPhase = ''
+            passel .
+          '';
+
+          installPhase = ''
+            cp -r /tmp/target $out
+          '';
+
+          buildInputs = [ passel ];
+        };
+    };
   };
 }
