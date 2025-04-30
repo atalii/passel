d8cd68955957a6285c9e34c614fee4568cc74328
Tali Auster <me@tali.network>
2025-04-30 09:27:01 -0700

    flake: don't use flakeutils

--- flake.nix
+++ flake.nix
@@ -1,38 +1,38 @@
 {
   inputs.alirenix.url = "github:atalii/alirenix";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, alirenix, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages =
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          default = alirenix.lib.buildAlireCrate ({
-            inherit pkgs;
-          }) {
-            src = ./.;
-            pname = "passel";
-            version = "0.1.0";
-            index = alirenix.packages.x86_64-linux.community-index;
-            alire = alirenix.packages.x86_64-linux.alire;
-            depsHash = "sha256-uljt5kgnMzzvT9rSl+vnQnNpKx0YSX9ZAVijV2puP8s=";
-          };
-
-          test-site = pkgs.callPackage self.lib.buildSite {
-            src = ./res;
-            passel = self.packages.${system}.default;
-            version = "0.1.0";
-          };
+  outputs = { self, alirenix, nixpkgs }:
+    let
+      systems = nixpkgs.lib.systems.flakeExposed;
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      define = f: forAllSystems (system: f nixpkgs.legacyPackages.${system});
+    in {
+      packages = define (pkgs: {
+        default = alirenix.lib.buildAlireCrate ({
+          inherit pkgs;
+        }) {
+          src = ./.;
+          pname = "passel";
+          version = "0.1.0";
+          index = alirenix.packages.x86_64-linux.community-index;
+          alire = alirenix.packages.x86_64-linux.alire;
+          depsHash = "sha256-uljt5kgnMzzvT9rSl+vnQnNpKx0YSX9ZAVijV2puP8s=";
         };
-      devShells.default =
-        let pkgs = nixpkgs.legacyPackages.${system};
-	in pkgs.mkShell {
-	  nativeBuildInputs = with pkgs; [
-	    alire gnat14 gnat14Packages.gprbuild
-	  ];
-	};
-    }) // {
+
+        test-site = pkgs.callPackage self.lib.buildSite {
+          src = ./res;
+          passel = self.packages.${pkgs.system}.default;
+          version = "0.1.0";
+        };
+      });
+
+      devShells.default = define (pkgs: pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          alire gnat14 gnat14Packages.gprbuild
+        ];
+      });
+    } // {
       lib = {
         buildSite =
           { src
