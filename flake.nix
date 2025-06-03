bec93bbb3897b294c534424279a234ac8f53017b
Tali Auster <me@tali.network>
2025-06-02 19:38:23 -0600

    chore: update flake deps

--- flake.nix
+++ flake.nix
@@ -1,24 +1,33 @@
 {
   inputs.alirenix.url = "github:atalii/alirenix";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
 
-  outputs = { self, alirenix, nixpkgs }:
+  outputs =
+    {
+      self,
+      alirenix,
+      nixpkgs,
+    }:
     let
       systems = nixpkgs.lib.systems.flakeExposed;
       forAllSystems = nixpkgs.lib.genAttrs systems;
       define = f: forAllSystems (system: f nixpkgs.legacyPackages.${system});
-    in {
+    in
+    {
       packages = define (pkgs: {
-        default = alirenix.lib.buildAlireCrate ({
-          inherit pkgs;
-        }) {
-          src = ./.;
-          pname = "passel";
-          version = "0.1.0";
-          index = alirenix.packages.x86_64-linux.community-index;
-          alire = alirenix.packages.x86_64-linux.alire;
-          depsHash = "sha256-uljt5kgnMzzvT9rSl+vnQnNpKx0YSX9ZAVijV2puP8s=";
-        };
+        default =
+          alirenix.lib.buildAlireCrate
+            ({
+              inherit pkgs;
+            })
+            {
+              src = ./.;
+              pname = "passel";
+              version = "0.1.0";
+              index = alirenix.packages.x86_64-linux.community-index;
+              alire = alirenix.packages.x86_64-linux.alire;
+              depsHash = "sha256-uljt5kgnMzzvT9rSl+vnQnNpKx0YSX9ZAVijV2puP8s=";
+            };
 
         test-site = pkgs.callPackage self.lib.buildSite {
           src = ./res;
@@ -27,20 +36,28 @@
         };
       });
 
-      devShells.default = define (pkgs: pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          alire gnat14 gnat14Packages.gprbuild
-        ];
-      });
-    } // {
+      devShells.default = define (
+        pkgs:
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            alire
+            gnat14
+            gnat14Packages.gprbuild
+          ];
+        }
+      );
+    }
+    // {
       lib = {
         buildSite =
-          { src
-          , passel
-          , pname ? "passel-site"
-          , version
-          , stdenv
-          }: stdenv.mkDerivation {
+          {
+            src,
+            passel,
+            pname ? "passel-site",
+            version,
+            stdenv,
+          }:
+          stdenv.mkDerivation {
             inherit pname version src;
 
             buildPhase = ''
@@ -52,7 +69,7 @@
             '';
 
             buildInputs = [ passel ];
-        };
+          };
       };
     };
 }
